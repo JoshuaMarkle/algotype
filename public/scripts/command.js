d8959890821288cd489c123d1126fe67eb733587
JoshuaMarkle <joshuamarkle25@gmail.com>
2023-11-25 14:33:11 -0500

    complete command prompt and nav bar

--- public/scripts/command.js
+++ public/scripts/command.js
@@ -1,69 +1,117 @@
-import { focusOnTypingTest } from "./main.js";
+import { switchGamemode, switchLanguage, updateTimeAmount, updateWordCount } from "./main.js";
 
-const allCommands = [
-  "Algorithms",
-  "Words",
-  "Python",
-  "C",
-  "C++",
-  "C#",
-  "JavaScript",
-  "Command Prompt",
-  "Themes",
-  "Time",
-  "More",
-  // Add all other commands here
-];
+let allCommandsData = []; // Global variable to store command data
+let currentCommand = null; // Variable to store the selected main command
+
+// List of functions for each command
+const commandHandlers = {
+    Gamemodes: function (subcommand) {
+        switchGamemode(subcommand.toLowerCase());
+    },
+    Languages: function (subcommand) {
+        switchLanguage(subcommand.toLowerCase());
+    },
+    Time: function (subcommand) {
+        updateTimeAmount(parseInt(subcommand.split(" ")[0], 10));
+    },
+    Words: function (subcommand) {
+        updateWordCount(parseInt(subcommand.split(" ")[0], 10));
+    },
+    Difficulty: function (subcommand) {
+        console.log("Difficulty selected:", subcommand);
+    },
+    Themes: function (subcommand) {
+        console.log("Themes selected:", subcommand);
+    },
+};
 
 document.addEventListener("DOMContentLoaded", function () {
-  var commandInput = document.querySelector(".command-prompt-open input");
-  var commandPromptOpen = document.querySelector(".command-prompt-open");
-  var commandButtons = commandPromptOpen.querySelectorAll(".command-button");
-  const openCommandPromptButton = document.getElementById(
-    "open-command-prompt"
-  );
+    fetch("../data/commands.json")
+        .then((response) => response.json())
+        .then((data) => {
+            allCommandsData = data; // Store the data in the global variable
+            updateCommandButtons(); // Initial update
+        })
+        .catch((error) => console.error("Error fetching the commands:", error));
 
-  function updateCommandButtons() {
-    const inputValue = commandInput.value.trim().toLowerCase();
+    var commandInput = document.querySelector(".command-prompt-open input");
+    var commandPromptOpen = document.querySelector(".command-prompt-open");
+    var commandButtons = commandPromptOpen.querySelectorAll(".command-button");
 
-    if (inputValue === "") {
-      // If input is empty or whitespace, use default commands
-      commandButtons.forEach((button, index) => {
-        button.innerHTML = allCommands[index]
-          ? `<span>${allCommands[index]}</span>`
-          : "";
-        button.style.display = button.textContent ? "block" : "none";
-      });
-    } else {
-      // Filter and format commands based on input
-      const filteredCommands = allCommands.filter((command) =>
-        command.toLowerCase().startsWith(inputValue)
-      );
+    function updateCommandButtons() {
+        const inputValue = commandInput.value.trim().toLowerCase();
+        let filteredCommands = [];
 
-      // Update buttons with formatted HTML
-      commandButtons.forEach((button, index) => {
-        if (filteredCommands[index]) {
-          const partMatched = filteredCommands[index].slice(
-            0,
-            inputValue.length
-          );
-          const partUnmatched = filteredCommands[index].slice(
-            inputValue.length
-          );
-          button.innerHTML = `<span class="correct">${partMatched}</span><span>${partUnmatched}</span>`;
-          button.style.display = "block";
+        if (currentCommand) {
+            // If a main command is selected, filter its subcommands
+            const subcommands = allCommandsData.find(
+                (cmd) => cmd.command === currentCommand
+            ).subcommands;
+            subcommands.forEach((subcommand) => {
+                if (subcommand.toLowerCase().startsWith(inputValue)) {
+                    filteredCommands.push(subcommand);
+                }
+            });
         } else {
-          button.innerHTML = "";
-          button.style.display = "none";
+            // Filter and format main commands based on input
+            allCommandsData.forEach((commandObj) => {
+                if (commandObj.command.toLowerCase().startsWith(inputValue)) {
+                    filteredCommands.push(commandObj.command);
+                }
+            });
         }
-      });
+
+        // Update buttons with formatted HTML
+        commandButtons.forEach((button, index) => {
+            if (index === 0) {
+                button.innerHTML = `<span class="correct">${filteredCommands[index]}</span>`;
+                button.style.display = "block";
+            } else if (filteredCommands[index]) {
+                const partMatched = filteredCommands[index].slice(0, inputValue.length);
+                const partUnmatched = filteredCommands[index].slice(inputValue.length);
+                button.innerHTML = `<span class="correct">${partMatched}</span><span>${partUnmatched}</span>`;
+                button.style.display = "block";
+            } else {
+                button.innerHTML = "";
+                button.style.display = "none";
+            }
+        });
     }
-  }
 
-  commandInput.addEventListener("input", updateCommandButtons);
+    commandInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            const firstItemText = commandButtons[0].textContent.trim();
 
-  commandInput.addEventListener("focus", function () {
-    commandPromptOpen.style.display = "flex";
-    updateCommandButtons();
-  });
+            if (currentCommand && commandHandlers[currentCommand]) {
+                // Call the handler for the main command with the selected subcommand
+                commandHandlers[currentCommand](firstItemText);
+                commandPromptOpen.style.display = "none"; // Close the command prompt
+                commandInput.value = ""; // Clear the input field
+                currentCommand = null; // Reset the current command
+            } else if (!currentCommand) {
+                // Handle main command selection
+                const firstCommandObj = allCommandsData.find(
+                    (cmd) => cmd.command === firstItemText
+                );
+                if (firstCommandObj) {
+                    currentCommand = firstItemText;
+                    commandInput.value = ""; // Reset input field
+                    updateCommandButtons();
+                }
+            } else {
+                // Close the command prompt if no valid subcommand is found
+                commandPromptOpen.style.display = "none";
+                commandInput.value = "";
+                currentCommand = null;
+            }
+        }
+    });
+
+    commandInput.addEventListener("input", updateCommandButtons);
+
+    commandInput.addEventListener("focus", function () {
+        currentCommand = null;
+        commandPromptOpen.style.display = "flex";
+        updateCommandButtons();
+    });
 });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,8 +7,8 @@ const typingArea = document.getElementById("input-area");
 const timeDisplay = document.getElementById("time");
 const wpmDisplay = document.getElementById("wpm");
 const accuracyDisplay = document.getElementById("accuracy");
-const restartButton = document.getElementById("restart-button");
 const textDisplay = document.getElementById("text-display");
+const restartButton = document.getElementById("restart-button");
 
 // Timer variables
 let startTime;
@@ -23,14 +23,16 @@ const commandPromptButton = document.getElementById("open-command-prompt");
 
 // Focused inputbox state system
 const States = {
-  TYPING_TEST: "typingTest",
-  COMMAND_PROMPT: "commandPrompt",
+    TYPING_TEST: "typingTest",
+    COMMAND_PROMPT: "commandPrompt",
 };
 let currentState = States.TYPING_TEST;
 
-// Default gamemode and language
+// Default game settings
 let currentGamemode = "algorithms";
 let currentLanguage = "python";
+let currentNumOfWords = 25;
+let currentTimeAmount = 60;
 let currentText = "Loading...";
 
 // Buttons
@@ -38,100 +40,136 @@ restartButton.addEventListener("click", () => switchLanguage(currentLanguage));
 commandPromptButton.addEventListener("click", () => focusOnCommandPrompt());
 
 export function getCurrentText() {
-  return currentText;
+    return currentText;
 }
 
 export function getElaspedTime() {
-  return elapsedTime.value;
+    return elapsedTime.value;
 }
 
 function startTest() {
-  if (startTime) return; // If the test has started, don't start again
-  startTime = new Date();
-  timerInterval = startTimer(elapsedTime, timeDisplay, typingArea, {
-    interval: timerInterval,
-  });
+    if (startTime) return; // If the test has started, don't start again
+    startTime = new Date();
+    timerInterval = startTimer(elapsedTime, timeDisplay, typingArea, {
+        interval: timerInterval,
+    });
 }
 
 async function restartTest() {
-  // Search database for random text
-  currentText = await getRandomFunction(currentGamemode, currentLanguage);
+    // Search database for random text
+    currentText = await getRandomFunction(currentGamemode, currentLanguage, currentNumOfWords);
 
-  // Reset timer
-  clearInterval(timerInterval);
-  timerInterval = null;
-  startTime = null;
-  elapsedTime.value = 0;
-  timerInterval = startTimer(elapsedTime, timeDisplay, typingArea, {
-    interval: timerInterval,
-  });
+    // Reset timer
+    clearInterval(timerInterval);
+    timerInterval = null;
+    startTime = null;
+    elapsedTime.value = 0;
+    timerInterval = startTimer(elapsedTime, timeDisplay, typingArea, {
+        interval: timerInterval,
+    });
 
-  // Reset typing area
-  typingArea.contentEditable = "true";
-  typingArea.innerText = "";
-  typingArea.focus();
+    // Reset typing area
+    typingArea.contentEditable = "true";
+    typingArea.innerText = "";
+    typingArea.focus();
 
-  // Reset live displays
-  timeDisplay.textContent = "0";
-  wpmDisplay.textContent = "0";
-  accuracyDisplay.textContent = "100";
-  setTextDisplay(textDisplay, currentText); // Set the initial display text
-}
-
-function switchLanguage(language) {
-  currentLanguage = language;
-  restartTest();
-}
-
-function switchGamemode(gamemode) {
-  currentGamemode = gamemode;
-  restartTest();
+    // Reset live displays
+    timeDisplay.textContent = "0";
+    wpmDisplay.textContent = "0";
+    accuracyDisplay.textContent = "100";
+    setTextDisplay(textDisplay, currentText); // Set the initial display text
 }
 
 // For every entered character, update the text display
 typingArea.addEventListener("input", () => {
-  if (!timerInterval) startTest();
-  updateTextDisplay(typingArea, textDisplay, currentText, timerInterval);
+    if (!timerInterval) startTest();
+    updateTextDisplay(typingArea, textDisplay, currentText, timerInterval);
 });
 
 // If the user clicks outside of the command prompt, focus on the typing test
 document.addEventListener("click", function (event) {
-  if (
-    !commandInput.contains(event.target) &&
-    !commandPromptOpen.contains(event.target) &&
-    !commandPromptButton.contains(event.target)
-  ) {
-    commandPromptOpen.style.display = "none";
-    focusOnTypingTest();
-  }
+    if (
+        !commandInput.contains(event.target) &&
+        !commandPromptOpen.contains(event.target) &&
+        !commandPromptButton.contains(event.target)
+    ) {
+        commandPromptOpen.style.display = "none";
+        focusOnTypingTest();
+    }
 });
 
 // Focus on typing test and command prompt using tab and escape respectively
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    focusOnCommandPrompt();
-  } else if (event.key === "Tab") {
-    focusOnTypingTest();
-    switchLanguage(currentLanguage);
-  }
+    if (event.key === "Escape") {
+        if (currentState === States.TYPING_TEST) {
+            focusOnCommandPrompt();
+        } else {
+            focusOnTypingTest();
+        }
+    } else if (event.key === "Tab") {
+        focusOnTypingTest();
+        switchLanguage(currentLanguage);
+    }
 });
 
 // Function to switch focus onto the main textbox
 function focusOnTypingTest() {
-  var commandPromptOpen = document.querySelector(".command-prompt-open");
-  commandPromptOpen.style.display = "none";
-  var commandInput = document.querySelector(".command-prompt-open input");
-  commandInput.value = "";
-  currentState = States.TYPING_TEST;
-  typingArea.focus();
+    var commandPromptOpen = document.querySelector(".command-prompt-open");
+    commandPromptOpen.style.display = "none";
+    var commandInput = document.querySelector(".command-prompt-open input");
+    commandInput.value = "";
+    currentState = States.TYPING_TEST;
+    typingArea.focus();
 }
 
 // Function to switch focus onto the command prompt
 function focusOnCommandPrompt() {
-  var commandPromptOpen = document.querySelector(".command-prompt-open");
-  commandPromptOpen.style.display = "flex";
-  currentState = States.COMMAND_PROMPT;
-  commandArea.focus();
+    var commandPromptOpen = document.querySelector(".command-prompt-open");
+    commandPromptOpen.style.display = "flex";
+    currentState = States.COMMAND_PROMPT;
+    commandArea.focus();
+}
+
+// NAVIGATION BUTTONS
+
+const algorithmsGamemode = document.getElementById("algorithms-gamemode");
+const wordsGamemode = document.getElementById("words-gamemode");
+
+algorithmsGamemode.addEventListener("click", () => switchGamemode("algorithms"));
+wordsGamemode.addEventListener("click", () => switchGamemode("words"));
+
+const pythonButton = document.getElementById("python-language");
+const cppButton = document.getElementById("cpp-language");
+const javascriptButton = document.getElementById("javascript-language");
+const csharpButton = document.getElementById("csharp-language");
+
+pythonButton.addEventListener("click", () => switchLanguage("python"));
+cppButton.addEventListener("click", () => switchLanguage("cpp"));
+csharpButton.addEventListener("click", () => switchLanguage("csharp"));
+javascriptButton.addEventListener("click", () => switchLanguage("javascript"));
+
+// GAME SETTINGS FUNCTIONS
+
+export function switchLanguage(language) {
+    currentLanguage = language;
+    restartTest();
+}
+
+export function switchGamemode(gamemode) {
+    currentGamemode = gamemode;
+    restartTest();
+}
+
+export function updateTimeAmount(timeAmount) {
+    currentGamemode = "time";
+    currentTimeAmount = timeAmount;
+    restartTest();
+}
+
+export function updateWordCount(numOfWords) {
+    currentGamemode = "words";
+    currentNumOfWords = numOfWords;
+    restartTest();
 }
 
 export { timerInterval, focusOnTypingTest };
